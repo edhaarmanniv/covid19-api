8812ff70bd3335c057b4c9f871c6e98fe811a029
edhaarmanniv <edward.haarmann.iv@gmail.com>
2020-07-08 21:28:13 -0500

    Defined pydantic models in schema.py for Global and US databases

--- app/schemas.py
+++ app/schemas.py
@@ -1,14 +1,35 @@
 from datetime import date
+from typing import Optional
 from pydantic import BaseModel
 
+class GlobalDailyCases(BaseModel):
+    
+    index : int 
+    country_region : str
+    province_state : str
+    lat : Optional[float]
+    long : Optional[float]
+    date : date
+    confirmed : Optional[int]
+    deaths : Optional[int]
+    recovered : Optional[int]
+    iso3 : str
 
-class Record(BaseModel):
-    id: int
-    date: date
-    country: str
-    cases: int
-    deaths: int
-    recoveries: int
+    class Config:
+        orm_mode = True
+
+
+class USADailyCases(BaseModel):
+    
+    index : int
+    country_region : str 
+    province_state : str
+    county_city : str
+    lat : Optional[float]
+    long : Optional[float]
+    date : date
+    confirmed : Optional[int]
+    deaths : Optional[int]
 
     class Config:
         orm_mode = True

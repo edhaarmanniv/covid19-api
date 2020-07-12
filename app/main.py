8a5b3ef23289d6b31dcd7d5e26842a5fd8154ae5
edhaarmanniv <edward.haarmann.iv@gmail.com>
2020-07-12 15:55:16 -0500

    Added title and description as a test to iso3 codes as well as dynamic max date

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 from typing import List
 from datetime import date, timedelta
 
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import FastAPI, Depends, HTTPException, Query
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
+from sqlalchemy import func
 from starlette.responses import RedirectResponse
 
 from . import models, schemas
@@ -11,8 +12,8 @@ from .database import SessionLocal, engine
 
 models.Base.metadata.create_all(bind=engine)
 
-start_date_default = date.fromisoformat("1970-01-01") 
-end_date_default = date.today() - timedelta(days=1) # need to integrate "most recent date" into route function
+start_date_default = date.fromisoformat("1970-01-01")
+# end_date_default = date.today() - timedelta(days=1) # need to integrate "most recent date" into route function
 iso3_default = "ALL"
 state_default = "ALL"
 county_default = "ALL"
@@ -46,14 +47,21 @@ def main():
 async def global_route(
     db: Session = Depends(get_db),
     start_date: date = start_date_default,
-    end_date: date = end_date_default,
-    iso3: str = iso3_default,
+    end_date: date = get_db().query(func.max(GlobalDailyCases.date)),
+    iso3: List[str] = Query(
+        iso3_default,
+        title="ISO3",
+        description="ISO3 Country Code - to query multiple countries include a 'iso3=' field for each one in the query string.",
+        max_length=3,
+    ),
 ):
-    results = db.query(models.GlobalDailyCases).filter(GlobalDailyCases.date.between(start_date, end_date))
+    results = db.query(models.GlobalDailyCases).filter(
+        GlobalDailyCases.date.between(start_date, end_date)
+    )
 
     if iso3 != iso3_default:
-        results = results.filter(GlobalDailyCases.iso3==iso3)
-    
+        results = results.filter(GlobalDailyCases.iso3 == iso3)
+
     return results.all()
 
 
@@ -66,12 +74,14 @@ async def usa_route(
     county: str = county_default,
 ):
 
-    results = db.query(models.USADailyCases).filter(USADailyCases.date.between(start_date, end_date))
+    results = db.query(models.USADailyCases).filter(
+        USADailyCases.date.between(start_date, end_date)
+    )
 
     if state != state_default:
-        results = results.filter(USADailyCases.province_state==state)
-    
+        results = results.filter(USADailyCases.province_state == state)
+
     if county != county_default:
-        results = results.filter(USADailyCases.county==county)
+        results = results.filter(USADailyCases.county == county)
 
     return results.all()

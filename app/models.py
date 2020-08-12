0bd8c4d9802bcb32bd8bf0c8381cf1c22ec74fb4
edkrueger <edkrueger@gmail.com>
2020-08-12 13:21:30 -0500

    Specify the VARCHAR length.

--- app/models.py
+++ app/models.py
@@ -9,23 +9,23 @@ from .database import Base
 class GlobalDailyCases(Base):
     __tablename__ = "daily_cases"
     index = Column(Integer, primary_key=True, nullable=False)
-    country_region = Column(String)
-    province_state = Column(String)
+    country_region = Column(String(255))
+    province_state = Column(String(255))
     lat = Column(Float)
     long = Column(Float)
     date = Column(Date)
     confirmed = Column(Integer)
     deaths = Column(Integer)
     recovered = Column(Integer)
-    iso3 = Column(String)
+    iso3 = Column(String(255))
 
 
 class USADailyCases(Base):
     __tablename__ = "usa_covid19"
     index = Column(Integer, primary_key=True)
-    country_region = Column(String)
-    province_state = Column(String)
-    county_city = Column(String)
+    country_region = Column(String(255))
+    province_state = Column(String(255))
+    county_city = Column(String(255))
     lat = Column(Float)
     long = Column(Float)
     date = Column(Date)
